fadbc405ad231ffe18a80540c2631e7ffb63175e
OgFishies <119113162+OgFishiesYT@users.noreply.github.com>
2023-08-05 23:49:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,13 +6,11 @@ var Title1
 var Title2
 var Title3
 
-
 function UpdateVideos() {
     console.log("Running")
     div1 = document.getElementById("1")
     div2 = document.getElementById("2")
     div3 = document.getElementById("3")
-    div4 = document.getElementById("4")
 
     titleele1 = div1.getElementsByTagName("h1")[0]
     titleele2 = div2.getElementsByTagName("h1")[0]
@@ -32,7 +30,7 @@ function UpdateVideos() {
 }
 
 function GetVideos() {
-    f = fetch("https://recroom2019.github.io/CommunityBoard.json",).then((a)=>{a.json().then((response) => {
+    f = fetch("https://ogfishiesyt.github.io/RR2020.github.io/CommunityBoard.json",).then((a)=>{a.json().then((response) => {
         video1 = response.Videos[0].BlobName
         video2 = response.Videos[1].BlobName
         video3 = response.Videos[2].BlobName
@@ -40,8 +38,6 @@ function GetVideos() {
         Title1= response.Videos[0].Title
         Title2 = response.Videos[1].Title
         Title3 = response.Videos[2].Title
-        
-        
         console.log("Done")
         UpdateVideos()
     })})
